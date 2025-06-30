d66d3c3cb994a2642d6ef9da3051d1dd5e368f26
hilalcakmak <cakmak.hilal@hotmail.com>
2025-06-30 03:04:55 +0300

    admin paneli + kategori crud

---
+++ eticaret/eticaret/Controllers/FavoriController.cs
@@ -0,0 +1,39 @@
+﻿using eticaret.Data;
+using Microsoft.AspNetCore.Mvc;
+
+public class FavoriController : Controller
+{
+    private readonly AppDbContext _context;
+    public FavoriController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // Favori ekle/çıkar (toggle)
+    public IActionResult Toggle(int id)
+    {
+        var kullaniciEmail = HttpContext.Session.GetString("KullaniciEmail");
+        if (string.IsNullOrEmpty(kullaniciEmail))
+            return RedirectToAction("Index", "Account");
+        var favoriler = HttpContext.Session.GetObject<List<int>>("favoriler") ?? new List<int>();
+        if (favoriler.Contains(id))
+            favoriler.Remove(id); // varsa çıkar
+        else
+            favoriler.Add(id);    // yoksa ekle
+
+        HttpContext.Session.SetObject("favoriler", favoriler);
+
+        var referer = Request.Headers["Referer"].ToString();
+        if (!string.IsNullOrEmpty(referer))
+            return Redirect(referer);
+        return RedirectToAction("Index", "Home");
+    }
+
+    // Favorilerim
+    public IActionResult Index()
+    {
+        var favoriler = HttpContext.Session.GetObject<List<int>>("favoriler") ?? new List<int>();
+        var urunler = _context.Urunler.Where(u => favoriler.Contains(u.UrunNo)).ToList();
+        return View(urunler);
+    }
+}
--- eticaret/eticaret/Controllers/KategoriController.cs
+++ eticaret/eticaret/Controllers/KategoriController.cs
@@ -1,28 +1,89 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using eticaret.Data;
-//using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using eticaret.Data;
+using eticaret.Models;
+using System.Linq;
 
-//// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace eticaret.Controllers
+{
+    public class KategoriController : Controller
+    {
+        private readonly AppDbContext _context;
+        public KategoriController(AppDbContext context)
+        {
+            _context = context;
+        }
 
-//namespace eticaret.Controllers
-//{
-//    public class KategoriMenuViewComponent : ViewComponent
-//{
-//    private readonly AppDbContext _context;
-//    public KategoriMenuViewComponent(AppDbContext context)
-//    {
-//        _context = context;
-//    }
+        // Listele
+        public IActionResult Index()
+        {
+            var kategoriler = _context.Kategoriler.ToList();
+            return View(kategoriler);
+        }
 
-//    public IViewComponentResult Invoke()
-//    {
-//        var kategoriler = _context.Kategoriler.ToList();
-//        return View(kategoriler);   // Model olarak gönderiyoruz
-//    }
-//}
+        // Ekle (GET)
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-//}
+        // Ekle (POST)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Kategori kategori)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Kategoriler.Add(kategori);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(kategori);
+        }
+
+        // Güncelle (GET)
+        public IActionResult Edit(int id)
+        {
+            var kategori = _context.Kategoriler.Find(id);
+            if (kategori == null) return NotFound();
+            return View(kategori);
+        }
+
+        // Güncelle (POST)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Kategori kategori)
+        {
+            if (id != kategori.KategoriNo) return NotFound();
+            if (ModelState.IsValid)
+            {
+                _context.Update(kategori);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(kategori);
+        }
+
+        // Sil (GET)
+        public IActionResult Delete(int id)
+        {
+            var kategori = _context.Kategoriler.Find(id);
+            if (kategori == null) return NotFound();
+            return View(kategori);
+        }
+
+        // Sil (POST)
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var kategori = _context.Kategoriler.Find(id);
+            if (kategori == null) return NotFound();
+
+            _context.Kategoriler.Remove(kategori);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
+
 
